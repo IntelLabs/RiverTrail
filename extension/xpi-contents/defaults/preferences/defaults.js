0994f8998728c48e68651f0c66fb77f4df1266c3
Lindsey Kuper <lindsey@composition.al>
2014-10-08 11:22:50 -0700

    Minor cleanup.
    "defaults.js" seems to be the conventional name for prefs.

---
+++ extension/xpi-contents/defaults/preferences/defaults.js
@@ -0,0 +1,2 @@
+pref("extensions.river-trail-extension.defaultPlatform", 0);
+pref("extensions.river-trail-extension.defaultDeviceType", 1);
--- extension/xpi-contents/defaults/preferences/dpopreferences.js
+++
@@ -1,2 +0,0 @@
-pref("extensions.dpointerface.defaultPlatform", 0);
-pref("extensions.dpointerface.defaultDeviceType", 1);
